6a66a76fd50237c5811ff6e58f8c8277645afcb2
Egor Filipas <filipasegor@gmail.com>
2023-08-17 19:39:20 +0300

    fddf

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,17 +2,10 @@ import styles from './globals.scss'
 import Link from 'next/link'
 import Image from 'next/image'
 
-// import Navbar from "../components/mains/Navbar"
-// import GlobalLayout from '@/components/mains/GlobalLayout'
+
 import LeftSide from '../components/mains/layout/LeftSide'
 import RightSide from '../components/mains/layout/RightSide'
 import Footer from '../components/mains/layout/Footer'
-
-// import { 
-//   LeftSide,
-//   RightSide,
-//   Footer
-// } from '@/components/index'
 
 
 export const metadata = {
